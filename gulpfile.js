4526424fd48efec4e28014de7eaae0c8c986dbdb
Tony.Liang <pillar0514@163.com>
2017-08-01 16:18:18 +0800

    完善静态文件路径配置

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,15 @@ const $gulpReplace = require('gulp-replace');
 
 $gulp.task('clean', () => $del(['./dist/']));
 
+$gulp.task('clean-test', () => $del(['./test/dist/']));
+
 $gulp.task('build', ['clean'], () => {
 	return $gulp.src([
 		'./src/*.js',
 		'./src/*._'
 	]).pipe(
 		$gulpReplace(
-			(/'assets/mg), '\'//sporeui.github.io/spore-documentation-theme/src/assets'
+			(/'assets/mg), '\'https://sporeui.github.io/spore-documentation-theme/src/assets'
 		)
 	).pipe($gulp.dest('./dist'));
 });
